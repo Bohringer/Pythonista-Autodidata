554cd344513ea252d6e6f06e1abdcb7dacfb92e2
Luiz Bohringer <luizp5022@gmail.com>
2024-03-25 22:52:54 -0300

    Create Funções.py

---
+++ Funções.py
@@ -0,0 +1,9 @@
+#Desafio 1
+def gerar_nome_completo(nome, sobrenome):
+    print(f'Bem-vindo(a) {nome} {sobrenome}')
+gerar_nome_completo('Luiz', 'Bohringer')
+
+#Desafio 2
+def calcular_valores(preco, quantidade=1):
+    print(preco*quantidade)
+calcular_valores(20, 2)
